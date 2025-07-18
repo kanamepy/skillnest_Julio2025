da3ff3d05f7bd0f4a652908e8cd0ef08a11bb50f
Vanesa Gomez <vanesa.saldivar89@gmail.com>
2025-07-17 21:19:43 -0300

    primer script

---
+++ script.js
@@ -0,0 +1,21 @@
+var edad= 19;
+var nombre= "ricardo";
+var estaLloviendo = false; 
+
+//==  !=
+//esta lloviendo == true  --> diferente no entra
+// estaLloviendo != true  --> diferente entra
+/*if(estalloviendo == true){
+    console.log("Esta lloviendo, lleva tu paraguas");
+}*/
+
+if(estalloviendo != true){
+    console.log(" Entro al if, Esta lloviendo, lleva tu paraguas");
+}
+
+//no se cumple esta condición, no ingresa
+if(estalloviendo == true){
+    console.log("Esta lloviendo, lleva tu paraguas");
+}
+
+
